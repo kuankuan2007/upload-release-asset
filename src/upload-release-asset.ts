2bd537cf440bb011425224aa78713f87a2b1c027
kuankuan <2163826131@qq.com>
2026-02-27 12:44:53 +0800

    init v0.1.0

---
+++ src/upload-release-asset.ts
@@ -0,0 +1,74 @@
+import * as core from '@actions/core';
+import { getOctokit } from '@actions/github';
+import fs from 'fs';
+import path from 'path';
+
+export default async function run() {
+  const owner = core.getInput('owner', { required: true });
+  const repo = core.getInput('repo', { required: true });
+  const releaseId = core.getInput('release_id', { required: true });
+  const files = core
+    .getInput('files', { required: true })
+    .split('\n')
+    .filter(Boolean)
+    .map((file) => path.resolve(file.trim()));
+  const contentType =
+    core.getInput('content_type', { required: false }) || 'application/octet-stream';
+
+  const token = process.env.GITHUB_TOKEN;
+
+  if (!token) {
+    core.setFailed('GITHUB_TOKEN is required');
+    return;
+  }
+
+  for (const file of files) {
+    if (
+      await fs.promises.access(file, fs.constants.R_OK).then(
+        () => true,
+        () => false
+      )
+    ) {
+      core.setFailed(`file ${file} is not readable`);
+      return;
+    }
+  }
+  const output: string[] = [];
+  for (const file of files) {
+    const data = await fs.promises.readFile(file, { encoding: 'utf8' });
+
+    const resItem = await upload({
+      owner,
+      repo,
+      release_id: Number(releaseId),
+      name: path.basename(file),
+      data,
+      token,
+      contentType,
+    });
+    output.push(resItem.data.browser_download_url);
+  }
+  core.setOutput('browser_download_urls', output.join('\n'));
+}
+export async function upload(options: {
+  owner: string;
+  repo: string;
+  release_id: number;
+  name: string;
+  data: string;
+  token: string;
+  contentType: string;
+}) {
+  const octokit = getOctokit(options.token);
+
+  return await octokit.rest.repos.uploadReleaseAsset({
+    owner: options.owner,
+    repo: options.repo,
+    release_id: options.release_id,
+    name: options.name,
+    data: options.data,
+    headers: {
+      'content-type': options.contentType,
+    },
+  });
+}
